b36f0e7cd96ed5895da0a5480cdf6d9293c19486
João Carlos <joaocarlos.cic1@gmail.com>
2023-04-11 00:00:48 -0300

    coordenation crud created

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-import { studentRouter, teacherRouter } from './routes';
+import { studentRouter, teacherRouter, coordenationRouter} from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/students', studentRouter);
 app.use('/teachers', teacherRouter);
+app.use('/coordenation', teacherRouter);
 
 export default app;
