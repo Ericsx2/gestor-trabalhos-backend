9c0bbce91427bbdd593eccaf3513794d8c426295
lucasao98 <lucasacramento32@gmail.com>
2023-04-23 17:14:25 -0300

    Matters was removed

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-import { studentRouter, teacherRouter, projectRouter, matterRouter } from './routes';
+import { studentRouter, teacherRouter, projectRouter } from './routes';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +10,5 @@ app.use(cors());
 app.use('/students', studentRouter);
 app.use('/teachers', teacherRouter);
 app.use('/projects', projectRouter);
-app.use('/matters', matterRouter);
 
 export default app;
