37bc1883fcd5f86a1e6381b78e11348290a7bec1
Eric Soares Silva <ericsoares51@hotmail.com>
2023-05-21 18:47:57 -0300

    fix: routes semantic

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 
-import { subjectRouter, projectRouter, authRouter, userRouter, firstAccessRouter } from './routes';
+import {
+  subjectRouter,
+  projectRouter,
+  authRouter,
+  userRouter,
+  firstAccessRouter,
+} from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/projects', projectRouter);
-app.use('/subjects', subjectRouter);
+app.use('/project', projectRouter);
+app.use('/subject', subjectRouter);
 app.use('/auth', authRouter);
 app.use('/firstAccess', firstAccessRouter);
-app.use('/users', userRouter);
-
+app.use('/user', userRouter);
 
 export default app;
