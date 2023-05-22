623fb3c81ae5ba06f28830a7c4fab83343569434
Eric Soares Silva <ericsoares51@hotmail.com>
2023-05-22 15:42:50 -0300

    fix: jwt contains role to autheticate

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,38 +1,41 @@
-import { Request, Response } from "express";
-import { prismaClient } from "../database/prismaClient";
-import { compareSync } from "bcrypt";
-import jwt from "jsonwebtoken";
+import { Request, Response } from 'express';
+import { prismaClient } from '../database/prismaClient';
+import { compareSync } from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 class AuthController {
-    async auth(request: Request, response: Response) {
-        const { email, password } = request.body;
+  async auth(request: Request, response: Response) {
+    const { email, password } = request.body;
 
-        const user = await prismaClient.user.findFirst({
-            where: {
-                email
-            }
-        });
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email,
+      },
+    });
 
-        if (!user) {
-            return response.status(404).send({ message: "Email ou senha inválido!" });
-        }
-
-        const passwordIsValid = compareSync(password, user.password);
-
-        if (!passwordIsValid) {
-            return response.status(404).send({ message: "Email ou senha inválido!" });
-        }
-
-        const token = jwt.sign({
-            id: user.id,
-            name: user.name,
-            email: user.email
-        },
-            process.env.JWT_SECRET as string,
-            { expiresIn: '24h' });
-
-        return response.send({ token });
+    if (!user) {
+      return response.status(404).send({ message: 'Email ou senha inválido!' });
     }
+
+    const passwordIsValid = compareSync(password, user.password);
+
+    if (!passwordIsValid) {
+      return response.status(404).send({ message: 'Email ou senha inválido!' });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '24h' }
+    );
+
+    return response.send({ token });
+  }
 }
 
 export { AuthController };
