fbbfdf77b92edeebf8016d7420cd162bb9f9792f
Eric Soares Silva <ericsoares51@hotmail.com>
2023-05-22 10:37:41 -0300

    build: removed hbs files from build

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src'],
+  entry: ['src', '!src/**/*.hbs'],
   splitting: false,
   minify: true,
   target: 'es2016',
